a0e17b7ed432efa00b6451fe13f7bdd66acbbe41
Aaron <43335377+ru19wu06@users.noreply.github.com>
2021-01-12 17:12:59 +0800

    費氏數列

---
+++ Arrays.go
@@ -0,0 +1,27 @@
+package main
+
+//結構範例
+//費氏數列
+
+import (
+    "fmt"
+)
+
+
+func main(){
+	scores := make([]int, 20)
+	//scores[7] = 669
+	scores[0]=1
+	scores[1]=1
+	for i:=0;i<15;i++{
+		scores[i+2]=scores[i]+scores[i+1]
+
+		fmt.Println(scores[i])
+	}
+
+	// scores = append(scores, 5)
+	// scores = append(scores, 9)
+	fmt.Println(scores) // prints [5]
+
+
+}
